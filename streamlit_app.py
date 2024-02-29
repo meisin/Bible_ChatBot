7d38f6a1846b02bffd0bbab7a0344744bcd5e6a7
Dr. Meisin Lee <meisin123@gmail.com>
2024-02-29 18:19:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.title('Ask me anything in the Bible!')
 with st.sidebar:
         st.image('./image/bible.jpg')
 
-st.sidebar.markdown("This chatbot is built as a project to demonstrate the ability of Large Language Models answering questions based on specific source of information (in this case the Bible). It does quite well in Comprehension-like questions BUT may not work so well on Theological questions. We strongly suggest that you direct these questions to a pastor or any church leader. ")
+st.sidebar.markdown("This chatbot is built as a project to demonstrate the ability of Large Language Models in answering questions based on specific sources of information (in this case the Bible). It does quite well in Comprehension-like questions BUT may not work so well on Theological questions. We strongly suggest that you direct these questions to a pastor or any church leader. ")
 st.sidebar.markdown("---")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat message history
