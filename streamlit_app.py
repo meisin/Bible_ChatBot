161a1283c958ab73c4067f488a219ea937ab6833
Dr. Meisin Lee <meisin123@gmail.com>
2024-02-29 14:09:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 #from llama_index import VectorStoreIndex, ServiceContext, Document
 #from llama_index.llms import OpenAI
-#import openai
+import openai
 #from llama_index import SimpleDirectoryReader
 
 #openai.api_key = st.secrets.openai_key
