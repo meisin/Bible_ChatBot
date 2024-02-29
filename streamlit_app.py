f471ae47d802c6444ec1c3d41b85bc322090518e
Dr. Meisin Lee <meisin123@gmail.com>
2024-02-29 14:00:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
 
+openai.api_key = st.secrets.openai_key
+st.header("Chat with the Holy Bible")
+
 st.title('🎈 App Name')
 
 st.write('Hello world!')
