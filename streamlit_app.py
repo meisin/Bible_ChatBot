0e8b4c170bbe23a4d7119cbe710badd3806f19f0
Dr. Meisin Lee <meisin123@gmail.com>
2024-02-29 13:59:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
 
 st.title('🎈 App Name')
 
