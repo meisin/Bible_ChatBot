c31e7cd6e9910cc31c98816a62304dff465faed2
Dr. Meisin Lee <meisin123@gmail.com>
2024-02-29 14:04:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
+#from llama_index import VectorStoreIndex, ServiceContext, Document
+#from llama_index.llms import OpenAI
 import openai
-from llama_index import SimpleDirectoryReader
+#from llama_index import SimpleDirectoryReader
 
 openai.api_key = st.secrets.openai_key
 st.header("Chat with the Holy Bible")
